9d8b810b90b0b6c287ff8d8056050649ee9676c2
krishnasaharsh-ncaa <krishna.saharsh@excidion.com>
2026-01-30 16:57:02 -0500

    Version with Line/Spread Output

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/NCAA-BB",
+  assetPrefix: "/NCAA-BB/",
   images: { unoptimized: true },
 };
 
